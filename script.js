862670081d0ade965e87efaec694397fd349c5c4
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-12-28 13:57:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height){
+		this.width=width;
+		this.height=height;
+	}
+	get getWidth(width){
+		return this.width;
+	}
+	get getHeight(height){
+		return this.height;
+	}
+	get getPerimeter(width, height){
+		return (this.width)*(this.height);
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor(width){
+		super (width,width);
+	}
+	getPerimeter(){
+		return 4*(this.width);
+	}
+}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
